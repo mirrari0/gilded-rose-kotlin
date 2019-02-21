962134d4c7701603db092abed51e23295fb8af56
Steven Combs <scombs@pillartechnology.com>
2019-02-21 14:16:52 -0600

    Updated common item to contain the zero logic as well as the double decrement logic

--- gilded-rose-kata/src/main/kotlin/CommonItem.kt
+++ gilded-rose-kata/src/main/kotlin/CommonItem.kt
@@ -2,5 +2,6 @@ data class CommonItem(val item: Item)
 
 fun CommonItem.updateItem() {
     if(item.quality > 0) item.quality -= 1
+    if(item.quality > 0 && item.sellIn <0) item.quality -= 1
     item.sellIn -= 1
 }
--- gilded-rose-kata/src/test/kotlin/CommonItemTest.kt
+++ gilded-rose-kata/src/test/kotlin/CommonItemTest.kt
@@ -13,10 +13,26 @@ class CommonItemTest {
     }
 
     @Test
-    fun `when updateItem is called on a common item with a sellin and quality of zero, it will decriment the sellIn by one but not decrement the quality`() {
-        val item = CommonItem(Item("Some Item", 0,0))
+    fun `when updateItem is called on a common item with a sellin greater than zero and quality of zero, it will decriment the sellIn by one but not decrement the quality`() {
+        val item = CommonItem(Item("Some Item", 12,0))
         item.updateItem()
-        assertEquals(item.item.sellIn, -1)
+        assertEquals(item.item.sellIn, 11)
+        assertEquals(item.item.quality, 0)
+    }
+
+    @Test
+    fun `when updateItem is called on a common item with a sellin less than zero and quality greater than one, it will decriment the sellIn by one and decrement the quality by two`() {
+        val item = CommonItem(Item("Some Item", -1,12))
+        item.updateItem()
+        assertEquals(item.item.sellIn, -2)
+        assertEquals(item.item.quality, 10)
+    }
+
+    @Test
+    fun `when updateItem is called on a common item with a sellin less than zero and quality of one, it will decriment the quality and sellIn by one each`() {
+        val item = CommonItem(Item("Some Item", -1,1))
+        item.updateItem()
+        assertEquals(item.item.sellIn, -2)
         assertEquals(item.item.quality, 0)
     }
 }
