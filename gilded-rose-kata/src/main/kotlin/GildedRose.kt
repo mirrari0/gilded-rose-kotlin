64cbcc6d211c152856654e20fff7ce8ae0146135
Steven Combs <scombs@pillartechnology.com>
2019-02-25 16:57:02 -0600

    refactored the main class

--- gilded-rose-kata/src/main/kotlin/GildedRose.kt
+++ gilded-rose-kata/src/main/kotlin/GildedRose.kt
@@ -1,61 +1,63 @@
 class GildedRose(
         val items: List<Item>) {
 
+    val SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros"
+    val CONCERT = "Backstage passes to a TAFKAL80ETC concert"
+    val AGED_BRIE = "Aged Brie"
+
 
     fun updateQuality() {
-        for (i in 0..items.size-1) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            }
-            else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    }
-                    else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                }
-                else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
+        items.forEach {
+            if (AGED_BRIE == it.name) {
+                handleAgedBrie(it)
+            } else if (CONCERT == it.name) {
+                handleConcert(it)
+            } else if (SULFURAS_HAND_OF_RAGNAROS != it.name) {
+                handleCommon(it)
             }
         }
+    }
 
+    private fun handleCommon(it: Item) {
+        decrementQuality(it)
+        it.sellIn = it.sellIn - 1
+        if (it.sellIn < 0) {
+            decrementQuality(it)
+        }
+    }
+
+    private fun decrementQuality(item: Item) {
+        if (item.quality > 0)
+            item.quality -= 1
+    }
+
+
+    private fun handleConcert(it: Item) {
+        incrementQuality(it)
+        if (it.sellIn < 11) {
+            incrementQuality(it)
+        }
+        if (it.sellIn < 6) {
+            incrementQuality(it)
+        }
+        it.sellIn -= 1
+        if (it.sellIn < 0) {
+            it.quality = 0
+        }
+
+    }
+
+    private fun incrementQuality(item: Item) {
+        if (item.quality < 50)
+            item.quality += 1
+    }
+
+    private fun handleAgedBrie(item: Item) {
+        incrementQuality(item)
+        item.sellIn -= 1
+        if (item.sellIn < 0) {
+            incrementQuality(item)
+        }
     }
 
 }
